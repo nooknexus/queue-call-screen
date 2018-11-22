2cce5cd56d4bc790b0b075171fd7db69e7c2605e
Phanupong Dongyen <phanupongnook@gmail.com>
2018-11-22 14:46:49 +0700

    update checkin

--- check_in/drug_checkin.php
+++ check_in/drug_checkin.php
@@ -1,3 +1,4 @@
+<?php require "../connect.php";?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -22,8 +23,7 @@ $(function () {
 });
 JS;
 
-
- ?>
+?>
 
     <div class="container">
 
@@ -47,26 +47,69 @@ JS;
         <div class="inputs">
           <label class="text-center">กรุณานำใบสลิปคิวของท่าน</label>
           <label class="text-center">สแกนที่หน้าเครื่อง</label>
-          
+
           <div class="input-group">
-                                        
+
                     <input class="inputs" id='barcode' name="barcode" type="text"  placeholder="" autocomplete="off">
                     <span class="input-group-addon" style="margin-top: 25px;background-color:white;border: 0px;"> <img src="../dist/img/supermarket-scanner.png" height="60px" /></span>
           </div>
-    
 
-          
+
+
             <?php
-            if (!empty($_POST['barcode'])) {
-                $barcode = $_POST['barcode'];
+if (!empty($_POST['barcode'])) {
+    $barcode = $_POST['barcode'];
+    $datethai = "select CONCAT( SUBSTRING( YEAR ( CURRENT_DATE ( ) ) + 543, 3, 2 ), DATE_FORMAT( CURRENT_DATE, '%m%d' ) ) curdate";
+    $query1 = mysqli_query($objCon, $datethai);
+    $result1 = mysqli_fetch_array($query1, MYSQLI_ASSOC);
+    $curdate = implode($result1);
+    //echo $curdate;
+
+    //  database exceute  here.
+    $sql = "SELECT * FROM ovst_queue_server WHERE vn = '$barcode' AND $curdate = SUBSTRING( vn, 1, 6 )";
+    $query = mysqli_query($objCon, $sql);
+    $result = mysqli_fetch_array($query, MYSQLI_ASSOC);
+    $vn = $result["vn"];
+    $hn = $result["hn"];
+    $fullname = $result["fullname"];
+    $depq = $result["depq"];
+
+    if ($vn != "") {
+        $sql2 = "SELECT * from ovst_queue_server_dep where vn = '$vn' and dep_visit = 'drug'";
+        $query2 = mysqli_query($objCon, $sql2);
+        $result2 = mysqli_fetch_array($query2, MYSQLI_ASSOC);
+        $dep_visit = $result2["dep_visit"];
+
+        if ($dep_visit == "drug") {
+            //echo "เช็คอินซ้ำ";
+            echo "<div class='alert alert-danger' role='alert'><h2><i class='fa fa-close' style='font-size:35px;color:red'></i> ขออภัยคุณเช็คอินไปแล้ว</h2></div>";
+        } else {
+            //echo "ยังไม่ได้เช็คอิน";
+            $msgdep = "ovst_queue-Drug-".$curdate;
+            //echo $msgdep;
+
+            $sql3 = "select get_serialnumber('$msgdep')";
+            $query3 = mysqli_query($objCon, $sql3);
+            $result3 = mysqli_fetch_array($query3, MYSQLI_ASSOC);
+            $serial = implode($result3);
+
+            $sql4 = "INSERT into ovst_queue_server_dep (vn,hn,dep_visit,dep,depq,fullname,time_visit,status) VALUES ('$vn','$hn','drug',LPAD('$serial', 4, '0'),'$depq','$fullname',CURRENT_TIME(),'1')";
+             
+            if (mysqli_query($objCon, $sql4)) {
+                $js = '';
                 echo "<div class='alert alert-success' role='alert'><h2><i class='fa fa-check-circle' style='font-size:35px;color:green'></i> ลงทะเบียนเรียบร้อยแล้ว</h2><p class='pull-right'> VN =  $barcode</p></div>";
-                //  database exceute  here.
-                if ($barcode==1){
-                  $js = '';
-                }
+            } else {
+                echo "Error: " . $sql4 . "<br>" . mysqli_error($objCon);
             }
-            ?>
-          
+
+        }
+
+    } else {
+        echo "<div class='alert alert-danger' role='alert'><h2><i class='fa fa-close' style='font-size:35px;color:red'></i> ไม่มีชื่อคุณในระบบคิว</h2></div>";
+    }
+}
+?>
+
           <div class="alert alert-info" role="alert">
                 <h2 class="text-center">กรุณารอเจ้าหน้าที่เรียกคิว</h2>
           </div>
@@ -93,7 +136,7 @@ JS;
 
       </script>
       <script>
-        <?=$js; ?>
+        <?=$js;?>
       </script>
       <!--javascript section-->
   </body>
